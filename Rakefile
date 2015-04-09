7397fcab641c1a8b21f3d0153163f69c818c0f2f
Chris Hanks <christopher.m.hanks@gmail.com>
2015-04-09 17:48:23 -0400

    Have the default rake task run specs.

--- Rakefile
+++ Rakefile
@@ -1,2 +1,3 @@
 require 'bundler/gem_tasks'
 
+Dir["./tasks/*.rb"].sort.each &method(:require)
---
+++ tasks/specs.rb
@@ -0,0 +1,5 @@
+require 'rspec/core/rake_task'
+
+RSpec::Core::RakeTask.new :default do |spec|
+  spec.pattern = './spec/**/*_spec.rb'
+end
