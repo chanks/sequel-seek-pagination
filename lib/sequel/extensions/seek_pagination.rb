926d03110f604a15d5214da9cade49c21450271e
Chris Hanks <christopher.m.hanks@gmail.com>
2015-04-09 17:45:19 -0400

    Support starting points in pagination.

--- lib/sequel/extensions/seek_pagination.rb
+++ lib/sequel/extensions/seek_pagination.rb
@@ -4,8 +4,30 @@ require 'sequel/extensions/seek_pagination/version'
 module Sequel
   class Dataset
     module SeekPagination
-      def seek_paginate(count)
-        limit(count)
+      def seek_paginate(count, after: nil)
+        ds = limit(count)
+
+        if after
+          column, direction = parse_column_and_direction(opts[:order].first)
+
+          case direction
+          when :asc  then ds.where{|r| r.>(r.__send__(column), after)}
+          when :desc then ds.where{|r| r.<(r.__send__(column), after)}
+          else raise "Bad direction!: #{direction.inspect}"
+          end
+        else
+          ds
+        end
+      end
+
+      private
+
+      def parse_column_and_direction(order)
+        case order
+        when Symbol                         then [order, :asc]
+        when Sequel::SQL::OrderedExpression then [order.expression, order.descending ? :desc : :asc]
+        else raise "Unrecognized order!: #{order.inspect}"
+        end
       end
     end
 
--- spec/seek_pagination_spec.rb
+++ spec/seek_pagination_spec.rb
@@ -4,6 +4,7 @@ describe Sequel::SeekPagination do
   it "should limit the dataset appropriately when a starting point is not given" do
     datasets = [
       DB[:seek].order(:id),
+      DB[:seek].order(:seek__id),
       DB[:seek].order(Sequel.asc(:id)),
       DB[:seek].order(Sequel.desc(:id)).reverse_order
     ]
@@ -16,5 +17,23 @@ describe Sequel::SeekPagination do
     # Then in reverse:
     result = DB[:seek].order(Sequel.desc(:id)).seek_paginate(30).all
     result.should == (9971..10000).map{|i| {id: i}}.reverse
+  end
+
+  it "should page properly when given a starting point" do
+    datasets = [
+      DB[:seek].order(:id),
+      DB[:seek].order(:seek__id),
+      DB[:seek].order(Sequel.asc(:id)),
+      DB[:seek].order(Sequel.desc(:id)).reverse_order
+    ]
+
+    datasets.each do |dataset|
+      result = dataset.seek_paginate(30, after: 79).all
+      result.should == (80..109).map{|i| {id: i}}
+    end
+
+    # Then in reverse:
+    result = DB[:seek].order(Sequel.desc(:id)).seek_paginate(30, after: 789).all
+    result.should == (759..788).map{|i| {id: i}}.reverse
   end
 end
